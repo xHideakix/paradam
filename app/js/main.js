98f6adb90286a88f3e6af1dbf587b735e3a8f756
xHideakix <astilus1997@gmail.com>
2020-05-19 14:40:59 +0300

    add new tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,14 @@
 $( document ).ready(function() {
   $('#selectionItemTwo').on('click', () => {
+    $('#selectionItemTwo').removeClass("isNotActive");
     $('#selectionItemTwo').addClass("activeSelection");
     $('#selectionItemOne').removeClass("activeSelection");
+    $('#selectionItemOne').addClass("isNotActive");
   });
   $('#selectionItemOne').on('click', () => {
-    $('#selectionItemOne').addClass("activeSelection");
     $('#selectionItemTwo').removeClass("activeSelection");
+    $('#selectionItemTwo').addClass("isNotActive");
+    $('#selectionItemOne').removeClass("isNotActive");
+    $('#selectionItemOne').addClass("activeSelection");
   });
 });
